281bc3a6619b0ad532c144dddd1b988a60cf82bf
Perceval FARAMAZ <perfaram@windowslive.com>
2018-03-07 19:25:51 +0100

    Added : PyDict_CheckIsDict (Check if a given Python object is a dictionary), PyDict_Enumerate (enumerate a Python dict), PyStringOrUnicode_Get_UTF8Buffer (get UTF8 representation of a Python string), PyErr_GetObject (get the current Python exception's associated object)

--- src/PySwift_ObjC/include/pyswift_macros.h
+++ src/PySwift_ObjC/include/pyswift_macros.h
@@ -1,9 +1,15 @@
 @import ObjectiveC.NSObjCRuntime;
+@import CoreFoundation.CFBase;
 @import Python;
+
+typedef bool (^PyDictEnumeratorBlock)(PyObject*__nonnull key, PyObject*__nonnull value, Py_ssize_t pos);
 
 PyObject*__nonnull PyNone_Get();
 
 bool PyList_CheckIsList(PyObject*__nonnull obj);
+
+bool PyDict_CheckIsDict(PyObject*__nonnull obj);
+void PyDict_Enumerate(PyObject*__nonnull dict, PyDictEnumeratorBlock _Nonnull block);
 
 PyObject*__nonnull PyList_Get_Item(PyObject*__nonnull seq, NSUInteger i);
 
@@ -11,3 +17,7 @@ PyObject*__nonnull PyTuple_Get_Item(PyObject*__nonnull seq, NSUInteger i);
 
 PyObject*__nonnull PyBool_True();
 PyObject*__nonnull PyBool_False();
+
+NSString*__nonnull PyStringOrUnicode_Get_UTF8Buffer(PyObject*__nonnull uniObj);
+
+PyObject*__nonnull PyErr_GetObject();
--- src/PySwift_ObjC/pyswift_macros.m
+++ src/PySwift_ObjC/pyswift_macros.m
@@ -1,10 +1,25 @@
 #import "include/pyswift_macros.h"
+@import Foundation.NSString;
 PyObject*__nonnull PyNone_Get() {
     Py_RETURN_NONE;
 }
 
 bool PyList_CheckIsList(PyObject*__nonnull obj) {
     return ((((((PyObject*)(obj))->ob_type))->tp_flags & (Py_TPFLAGS_LIST_SUBCLASS)) != 0);
+}
+
+bool PyDict_CheckIsDict(PyObject*__nonnull obj) {
+    return PyDict_Check(obj);
+}
+
+void PyDict_Enumerate(PyObject*__nonnull dict, PyDictEnumeratorBlock block) {
+    PyObject *key, *value;
+    Py_ssize_t pos = 0;
+    
+    while (PyDict_Next(dict, &pos, &key, &value)) {
+        bool shouldContinue = block(key, value, pos);
+        if (!shouldContinue) break;
+    }
 }
 
 PyObject*__nonnull PyList_Get_Item(PyObject*__nonnull seq, NSUInteger i) {
@@ -23,4 +38,33 @@ PyObject*__nonnull PyBool_True() {
 
 PyObject*__nonnull PyBool_False() {
     return Py_False;
+}
+
+NSString*__nonnull PyStringOrUnicode_Get_UTF8Buffer(PyObject*__nonnull string) {
+    NSString* ret = nil;
+    
+    PyObject* utf8 = PyUnicode_AsUTF8String(string);
+    if (!utf8) {
+        PyErr_Clear();
+        PyObject* ascii_mystring = PyUnicode_AsASCIIString(string);
+        ret = [[NSString alloc]
+               initWithBytes:PyBytes_AsString(ascii_mystring)
+               length:(NSUInteger)PyBytes_GET_SIZE(ascii_mystring)
+               encoding:NSUTF8StringEncoding];
+        Py_DECREF(ascii_mystring);
+    } else {
+        ret = [[NSString alloc]
+                      initWithBytes:PyBytes_AS_STRING(utf8)
+                      length:(NSUInteger)PyBytes_GET_SIZE(utf8)
+                      encoding:NSUTF8StringEncoding];
+        Py_DECREF(utf8);
+    }
+    return ret;
+}
+
+PyObject*__nonnull PyErr_GetObject() {
+    PyThreadState *tstate = PyThreadState_GET();
+    PyObject *value = tstate->curexc_value;
+    Py_XINCREF(value);
+    return ((PyBaseExceptionObject*)value)->args;
 }
